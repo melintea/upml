0dc755136eeb637ce368021066e8d2f74eb9bce9
Aurelian MELINTE <ame01@gmx.net>
2024-12-11 10:28:23 -0500

    test stuff

---
+++ src/_test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#
+# Usage: _test.sh file.plantuml
+# Verify file is syntactically parseable
+#
+#set -x
+
+echo "--- $1"
+
+pumlfile="$1" 
+
+#
+#
+#
+
+tlafile="${pumlfile%.*}.tla"
+
+./upml \
+    --in "$pumlfile" \
+    --backend tla \
+    --out "$tlafile" \
+    2>&1  || exit 1
+
+tlahome=${HOME}/tla/toolbox
+tlatoolbox=${tlahome}/toolbox
+java -cp ${tlahome}/tla2tools.jar pcal.trans "$tlafile" 
+
+#
+#
+#
+
+spinfile="${pumlfile%.*}.promela"
+
+./upml \
+    --in "$pumlfile" \
+    --backend spin \
+    --out "$spinfile" \
+    2>&1  || exit 1
+
+exespin=${HOME}/work/Spin/Src/spin
+rm pan.*
+${exespin} -a "$spinfile" || exit 1
+if [[ ! -f pan.c ]]; then
+    exit 1
+fi
+rm pan.*
+
+echo "---ok: $1"
