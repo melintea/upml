c63b5e8fa7f4922b547ea7dd93111edd95c96f82
Aurelian MELINTE <ame01@gmx.net>
2025-04-07 08:55:22 -0400

    (no)trace tests

--- plantuml/trace/_trace.sh
+++ plantuml/trace/_trace.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 #set -x
 
+exespin=${HOME}/work/Spin/Src/spin
 xexespin=${HOME}/work/Spin/optional_gui/ispin.tcl
 xdiffer=meld
 spinfile=trace.promela
 
-rm *.trail
+#rm *.trail
 ${xexespin} "$spinfile"&
 #${xdiffer} "$spinfile" &
+
+# safety
+rm pan pan.* _spin_nvr.tmp *.trail
+${exespin} -a "$spinfile" || exit 1
+if [[ ! -f pan.c ]]; then
+    exit 1
+fi
+gcc -DMEMLIM=1024 -O2 -DXUSAFE -DSAFETY -w -o pan pan.c || exit 1
+./pan -m10000 || exit 1
+rm pan pan.* _spin_nvr.tmp *.trail
+
+# acceptance
+${exespin} -a "$spinfile" || exit 1
+if [[ ! -f pan.c ]]; then
+    exit 1
+fi
+gcc -DMEMLIM=1024 -O2 -DXUSAFE -w -o pan pan.c || exit 1
+./pan -m10000 -a || exit 1
+rm pan pan.* _spin_nvr.tmp *.trail
+
+printf "\e[42m*** Pass ***\e[0m \n"
+
