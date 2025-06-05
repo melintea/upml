cc5a4dfe92f2b41453bd4dcf5b1a81bc8242f942
Aurelian MELINTE <ame01@gmx.net>
2025-06-05 08:34:06 -0400

    Samek stuff

---
+++ plantuml/hsm/_samek.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+pushd ../../src
+#make test || exit 1
+make || exit 1
+popd
+
+../../src/upml --in samek.plantuml --backend spin-hsm | tee samek.promela
+
+rm *.tmp *.trail
+xexespin=${HOME}/work/Spin/optional_gui/ispin.tcl
+${xexespin} samek.promela &
+
+../../_plantuml.sh samek.plantuml&
+#lximage-qt ./samek.png &
+nedit ./samek.promela &
+nedit ./samek.promela &
+
